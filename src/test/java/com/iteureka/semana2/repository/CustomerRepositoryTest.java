a50e3603710d30a786d53ec870eebf44e000099b
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-10-11 23:37:46 -0300

    Se arregla el problema con los test

--- src/test/java/com/iteureka/semana2/repository/CustomerRepositoryTest.java
+++ src/test/java/com/iteureka/semana2/repository/CustomerRepositoryTest.java
@@ -3,6 +3,7 @@ package com.iteureka.semana2.repository;
 import com.iteureka.semana2.model.CustomerEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerRepositoryTest {
 
     @Autowired
