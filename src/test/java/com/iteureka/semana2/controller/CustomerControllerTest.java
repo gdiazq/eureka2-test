46f2f078e534d221764a282183483fab1848f9bb
Guillermo Diaz <guillermoe.diazq@gmail.com>
2024-10-11 22:19:37 -0300

    Inicio de projecto semana2 QA con ProjectControllerTest

--- src/test/java/com/iteureka/semana2/controller/CustomerControllerTest.java
+++ src/test/java/com/iteureka/semana2/controller/CustomerControllerTest.java
@@ -12,6 +12,7 @@ import java.util.Collections;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 class CustomerControllerTest {
@@ -27,16 +28,16 @@ class CustomerControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
+    // Prueba para el método que obtiene todos los clientes
     @Test
     void testGetAllCustomer() {
         when(customerService.findAll()).thenReturn(Collections.emptyList());
-
         Iterable<CustomerEntity> customers = customerController.getAllCustomer();
-
         assertNotNull(customers);
         verify(customerService, times(1)).findAll();
     }
 
+    // Prueba para el método que obtiene un cliente por su ID
     @Test
     void testGetCustomerById() {
         Long id = 1L;
@@ -44,31 +45,27 @@ class CustomerControllerTest {
         customer.setId(id);
 
         when(customerService.findById(id)).thenReturn(Optional.of(customer));
-
         Optional<CustomerEntity> foundCustomer = customerController.getCustomerById(id);
-
         assertTrue(foundCustomer.isPresent());
         assertEquals(id, foundCustomer.get().getId());
         verify(customerService, times(1)).findById(id);
     }
 
+    // Prueba para el método que guarda un cliente
     @Test
     void testSaveCustomer() {
         CustomerEntity customer = new CustomerEntity();
         when(customerService.save(any(CustomerEntity.class))).thenReturn(customer);
-
         CustomerEntity savedCustomer = customerController.saveCustomer(customer);
-
         assertNotNull(savedCustomer);
         verify(customerService, times(1)).save(customer);
     }
 
+    // Prueba para el método que elimina un cliente por su ID
     @Test
     void testDeleteCustomerById() {
         Long id = 1L;
-
         customerController.deleteCustomerById(id);
-
         verify(customerService, times(1)).deleteById(id);
     }
 }
